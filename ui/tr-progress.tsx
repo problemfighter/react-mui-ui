6c10734672822530e41793df06f3309216a58381
Touhid Mia <touhid@bitmascot.com>
2019-07-24 07:26:54 +0600

    progress

--- ui/tr-progress.tsx
+++ ui/tr-progress.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 
 export const TRProgress = {
-    linear: (state: boolean, color: any = "primary", zIndex: any = 9999) => {
+    linear: (state: boolean, color: any = "primary", zIndex: any = 100) => {
         return (
             state ? (
                 <React.Fragment>
                     <Fade in={state}>
-                        <LinearProgress color={color} style={{ position: 'absolute', top: 64, zIndex: zIndex, left: 0, right: 0 }}/>
+                        <LinearProgress color={color} style={{ position: 'absolute', top: 0, zIndex: zIndex, left: 0, right: 0 }}/>
                     </Fade>
                 </React.Fragment>) : ""
         );

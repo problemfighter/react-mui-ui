ab0ad6015f905a823b2ccc886993381e97fdafe8
Touhid Mia <touhid@bitmascot.com>
2019-07-26 14:06:12 +0600

    Constant

--- ui/tr-pagination.tsx
+++ ui/tr-pagination.tsx
@@ -4,36 +4,46 @@ import React from "react";
 import {TablePagination} from "./ui-component";
 
 
-export default class TRPagination extends TRReactComponent<TRProps, TRState> {
+export interface TRPaginationAction {
+    nextPrevious(event: any, offset: number): void
+    onChangeItemPerPage(event: any): void
+}
+
+interface Props extends TRProps {
+    total: number
+    itemPerPage: number
+    page: number
+    trPaginationAction: TRPaginationAction
+    itemPerPageDropdown?: Array<number>
+    itemPerPageLabel?: string
+}
 
 
-    handleChangePage = (event:any, page:any) => {
-        console.log("handleChangePage " +page)
+export default class TRPagination extends TRReactComponent<Props, TRState> {
+
+    static defaultProps = {
+        itemPerPageLabel: "Item Per Page",
+        itemPerPageDropdown: [10, 20, 50, 100, 200]
     };
 
-    handleChangeRowsPerPage (event:any) {
-        console.log("handleChangeRowsPerPage " + event.target.value)
-    };
-
-    render(){
+    render() {
+        const {total, itemPerPage, page, trPaginationAction, itemPerPageLabel, itemPerPageDropdown} = this.props;
         return (
             <React.Fragment>
-            <TablePagination
-                rowsPerPageOptions={[5, 10, 25]}
-                component="div"
-                count={100}
-                rowsPerPage={10}
-                page={1}
-                backIconButtonProps={{
-                    'aria-label': 'Previous Page',
-                }}
-                nextIconButtonProps={{
-                    'aria-label': 'Next Page',
-                }}
-                labelRowsPerPage="Label Rows Per Page"
-                onChangePage={(event:any, page:any) => {this.handleChangePage(event, page)}}
-                onChangeRowsPerPage={(event:any) => {this.handleChangeRowsPerPage(event)}}
-            />
-        </React.Fragment>);
+                <TablePagination
+                    rowsPerPageOptions={itemPerPageDropdown}
+                    component="div"
+                    count={total}
+                    rowsPerPage={itemPerPage}
+                    page={page}
+                    labelRowsPerPage={itemPerPageLabel}
+                    onChangePage={(event: any, page: any) => {
+                        trPaginationAction.nextPrevious(event, page)
+                    }}
+                    onChangeRowsPerPage={(event: any) => {
+                        trPaginationAction.onChangeItemPerPage(event)
+                    }}
+                />
+            </React.Fragment>);
     }
 }

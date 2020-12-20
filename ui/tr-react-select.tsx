702ef02c3d4e91dafb112c30c4f08f2d0d8c07c8
hmtmcse <hmtm.cse@gmail.com>
2020-12-21 00:20:21 +0600

    updated

--- ui/tr-react-select.tsx
+++ ui/tr-react-select.tsx
@@ -1,24 +1,9 @@
 import React, {CSSProperties, HTMLAttributes} from 'react';
-import clsx from 'clsx';
 import Select from 'react-select';
 import {createStyles, Theme} from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
 import NoSsr from '@material-ui/core/NoSsr';
-import TextField, {BaseTextFieldProps} from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
-import Chip from '@material-ui/core/Chip';
-import MenuItem from '@material-ui/core/MenuItem';
-import CancelIcon from '@material-ui/icons/Cancel';
-import {ValueContainerProps} from 'react-select/src/components/containers';
-import {ControlProps} from 'react-select/src/components/Control';
-import {MenuProps, NoticeProps} from 'react-select/src/components/Menu';
-import {MultiValueProps} from 'react-select/src/components/MultiValue';
-import {OptionProps} from 'react-select/src/components/Option';
-import {PlaceholderProps} from 'react-select/src/components/Placeholder';
-import {SingleValueProps} from 'react-select/src/components/SingleValue';
-import {Omit} from '@material-ui/types';
 import TRReactComponent from 'tm-react/src/artifacts/framework/tr-react-component';
-import {Box, withStyles} from 'react-mui-ui/ui/ui-component';
+import {withStyles} from 'react-mui-ui/ui/ui-component';
 import {TRProps, TRState} from 'tm-react/src/artifacts/model/tr-model';
 
 export const ReactSelectStyles = (theme: Theme) =>
@@ -73,121 +58,6 @@ export const ReactSelectStyles = (theme: Theme) =>
             height: theme.spacing(2),
         },
     });
-
-
-// function NoOptionsMessage(props: NoticeProps<OptionType>) {
-//     return (
-//         <Typography
-//             color="textSecondary"
-//             className={props.selectProps.classes.noOptionsMessage}
-//             {...props.innerProps}>
-//             {props.children}
-//         </Typography>
-//     );
-// }
-//
-// type InputComponentProps = Pick<BaseTextFieldProps, 'inputRef'> & HTMLAttributes<HTMLDivElement>;
-//
-// function inputComponent({inputRef, ...props}: InputComponentProps) {
-//     return <div ref={inputRef} {...props} />;
-// }
-//
-// function Control(props: ControlProps<OptionType>) {
-//     const {
-//         children,
-//         innerProps,
-//         innerRef,
-//         selectProps: {classes, TextFieldProps},
-//     } = props;
-//
-//     return (
-//         <TextField
-//             fullWidth
-//             InputProps={{
-//                 inputComponent,
-//                 inputProps: {
-//                     className: classes.input,
-//                     ref: innerRef,
-//                     children,
-//                     ...innerProps,
-//                 },
-//             }}
-//             {...TextFieldProps}
-//         />
-//     );
-// }
-//
-// function Option(props: OptionProps<OptionType>) {
-//     return (
-//         <MenuItem
-//             ref={props.innerRef}
-//             selected={props.isFocused}
-//             component="div"
-//             style={{
-//                 fontWeight: props.isSelected ? 500 : 400,
-//             }}
-//             {...props.innerProps}>
-//             {props.children}
-//         </MenuItem>
-//     );
-// }
-//
-// type MuiPlaceholderProps = Omit<PlaceholderProps<OptionType>, 'innerProps'> &
-//     Partial<Pick<PlaceholderProps<OptionType>, 'innerProps'>>;
-//
-// function Placeholder(props: MuiPlaceholderProps) {
-//     const {selectProps, innerProps = {}, children} = props;
-//     return (
-//         <Typography color="textSecondary" className={selectProps.classes.placeholder} {...innerProps}>
-//             {children}
-//         </Typography>
-//     );
-// }
-//
-// function SingleValue(props: SingleValueProps<OptionType>) {
-//     return (
-//         <Typography className={props.selectProps.classes.singleValue} {...props.innerProps}>
-//             {props.children}
-//         </Typography>
-//     );
-// }
-//
-// function ValueContainer(props: ValueContainerProps<OptionType>) {
-//     return <div className={props.selectProps.classes.valueContainer}>{props.children}</div>;
-// }
-//
-// function MultiValue(props: MultiValueProps<OptionType>) {
-//     return (
-//         <Chip
-//             tabIndex={-1}
-//             label={props.children}
-//             className={clsx(props.selectProps.classes.chip, {
-//                 [props.selectProps.classes.chipFocused]: props.isFocused,
-//             })}
-//             onDelete={props.removeProps.onClick}
-//             deleteIcon={<CancelIcon {...props.removeProps} />}
-//         />
-//     );
-// }
-//
-// function Menu(props: MenuProps<OptionType>) {
-//     return (
-//         <Paper square className={props.selectProps.classes.paper} {...props.innerProps}>
-//             {props.children}
-//         </Paper>
-//     );
-// }
-//
-// const components = {
-//     Control,
-//     Menu,
-//     MultiValue,
-//     NoOptionsMessage,
-//     Option,
-//     Placeholder,
-//     SingleValue,
-//     ValueContainer,
-// };
 
 
 
